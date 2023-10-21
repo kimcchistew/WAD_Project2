be939cb69fab2a29845aeaec20265e25ef45fe78
kimcchistew <116082894+kimcchistew@users.noreply.github.com>
2023-10-21 11:48:08 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Latihan 2</title>
+</head>
+<body>
+    <h1>Latihan 2</h1>
+    <form method="post" action="proses.php">
+        <label for="angka">Masukkan Angka:</label>
+        <input type="text" name="angka" id="angka">
+        <br><br><br>
+        
+        <label for="pilihan">Pilih Operasi:</label>
+        <select name="pilihan" id="pilihan">
+            <option value="pilihan1">Penambahan</option>
+            <option value="pilihan2">Pengurangan</option>
+            <option value="pilihan3">Perkalian</option>
+            <option value="pilihan3">Pembagian</option>
+        </select>
+        <br><br><br>
+
+        <button type="button" class="btn btn-success">Hitung</button>
+    </form>
+
+</body>
+</html>
